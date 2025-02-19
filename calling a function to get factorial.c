2fac7698e65c4c63e9f63f7ba4307d5315352fd1
Aminur Rashid <aminurrashid4338@gmail.com>
2025-02-18 22:27:24 -0800

    yeah

---
+++ calling a function to get factorial.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int factorial(int N)
+{
+    int fact=1;
+    for(int i=1;i<=N;i++)
+    {
+        fact *=i;
+    }
+    return fact;
+}
+int main()
+{
+    int X;
+    scanf("%d",&X);
+    printf("factorial of %d is %d\n",X,factorial(X));
+}
