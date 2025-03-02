c31ffd10f4caaf762fab69c4a79aa5517175dc16
ghostfreak <aminur.sohag.251@northsouth.edu>
2025-03-03 02:05:59 +0600

    idea

---
+++ arry 1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+  int arr[10],N;
+  for(int N=0;N<10;N++)
+  {
+    printf("arr[%d]:",N);
+    scanf("%d",&arr[N]);
+    printf("%d\n",arr[N]);
+  }
+
+
+
+  return 0;
+}
