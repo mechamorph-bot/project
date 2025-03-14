33aa3e7baf42796aef37cfc682f41d55adcef57d
aminur <aminur.sohag.251@northsouth.edu>
2025-03-15 03:42:10 +0600

    gets puts

---
+++ gets and puts.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+  char a[100];
+  char b[100];
+
+
+  printf("\n enter a string of character less than 100 with space:");
+  gets("%c",&a);
+
+  printf("\n you have entered");
+  puts("%s",a[]);
+
+}
