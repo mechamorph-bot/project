a7ab38874c9aa529353d5868bf08fc9801fc00c7
Aminur Rashid <aminurrashid4338@gmail.com>
2025-05-10 20:47:48 -0700

    yiy

--- libery of string.c
+++ libery of string.c
@@ -2,22 +2,52 @@
 int stringlenth(char a[]);
 int stringCompare(char a[],char b[]);
 void stringcopy(char x[],char y[]);
-void stringconcat(char a[].char b[])
+void stringConcat(char a[],char b[]);
+int stringsearch(char a[],char ch)
 {
+    int isfound =0;
     int i=0;
-    for(i=0;i<a[i];i++)
+    while(a[i]!=0)
     {
-
+        if(a[i]==ch)
+        {
+            isfound=1;
+            break;
+        }
+        i++;
     }
+    return isfound;
 }
 int main()
 {
-
-
-    char b[]="hello";
-    char a[]="hello";
+    char b[100]="hello";
+    char a[100]="hello";
     char d[100];
-    printf("string b is copied to srting d which is %s\n",a,d);
+    char x;
+    printf("enter what you want to search:");
+    scanf("%c",&x);
+    if(stringsearch(a,x))
+    {
+        printf("character found\n");
+
+    }
+    else{
+        printf("not found\n");
+    }
+
+
+
+    stringConcat(a,b);
+    printf("concated strins is : %s\n",a);
+
+
+
+
+    stringcopy(b,d);
+    printf("string b is copied to srting d which is %s\n",d);
+
+
+
     if(stringCompare(a,b))
     {
         printf("strings are equal\n");
@@ -26,15 +56,32 @@ int main()
     {
         printf("strings are not equal\n");
     }
+
+
+
      int c=stringlenth(b);
     printf("\n%d",c);
 
   return 0;
 }
+void stringConcat(char a[],char b[])
+{
+    int p =stringlenth(a);
+    a[p]=' ';
+    p++;
+    int i=0;
+    while(b[i] !='\0')
+    {
+        a[p]=b[i];
+        p++;
+        i++;
+    }
+    a[p]='\0';
+}
 void stringcopy(char x[],char y[])
 {
     int i=0;
-    while(y[i]!='\0')
+    while(x[i]!='\0')
     {
         y[i]=x[i];
         i++;
